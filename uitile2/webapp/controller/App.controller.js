1be76bfb97765cf5d298c348f181a4623f1172b0
merengadas <ltaberna@bit.com.ar>
2023-12-20 19:45:01 +0000

    tile 2 added

---
+++ uitile2/webapp/controller/App.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define(
+    [
+        "sap/ui/core/mvc/Controller"
+    ],
+    function(BaseController) {
+      "use strict";
+  
+      return BaseController.extend("nptile2.uitile2.controller.App", {
+        onInit: function() {
+        }
+      });
+    }
+  );
+  
---
+++ uitile2/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"nptile2/uitile2/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
