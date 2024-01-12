f4f2ab4145e7879554b9305322b0171fbf6427f1
merengadas <ltaberna@bit.com.ar>
2024-01-12 20:12:15 +0000

    Agrego Dinamic Tile sin el Backend. Solo uso el destination para comunicarme

---
+++ dinamictile/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["html5/dinamictile/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ dinamictile/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"html5/dinamictile/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
